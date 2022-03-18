81368a8c15baea20fdedc0837e5493f54574134c
Soroosh Sadeghian <soroosh.sadeghian@divar.ir>
2022-03-18 18:51:29 +0330

    Add First Three Lectures

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+init_mathjax = function() {
+    if (window.MathJax) {
+    // MathJax loaded
+        MathJax.Hub.Config({
+            TeX: {
+                equationNumbers: {
+                autoNumber: "AMS",
+                useLabelIds: true
+                }
+            },
+            tex2jax: {
+                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+                processEscapes: true,
+                processEnvironments: true
+            },
+            displayAlign: 'center',
+            CommonHTML: {
+                linebreaks: { 
+                automatic: true 
+                }
+            },
+            "HTML-CSS": {
+                linebreaks: { 
+                automatic: true 
+                }
+            }
+        });
+    
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }
+}
+init_mathjax();
