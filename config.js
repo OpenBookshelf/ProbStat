e6d95812855e3fc80b74b1334df36d8ee3e05fce
Shahrzad Javidi <100502562+ShahrzadJavidi@users.noreply.github.com>
2022-08-13 10:38:03 +0430

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.config = {
     summary: "متن خلاصه توضیح دوره",
     aboutText: "راستی نظرات‌تون رو حتما با ما در میون بگذارید",
     contactEmail: "sh.javidi98@ut.ac.ir",
-    source: "منبع تدریس کتاب A FIRST COURSE IN PROBABILITY
+    source: "منبع درس کتاب A FIRST COURSE IN PROBABILITY
 نوشته Sheldon Ross می‌باشد",
     sessions: 23,
   },
