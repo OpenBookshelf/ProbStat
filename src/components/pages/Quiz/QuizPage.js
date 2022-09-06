608520640443015900f24671b3d10c3d4cd03ee2
pasadi <parna80as@gmail.com>
2022-09-06 13:04:44 +0430

    add score to quiz

--- src/components/pages/Quiz/QuizPage.js
+++ src/components/pages/Quiz/QuizPage.js
@@ -12,6 +12,7 @@ const QuizPage = () => {
   const [result, setResult] = React.useState();
 
   const [answers, setAnswers] = React.useState({});
+  console.log("🚀 ~ file: QuizPage.js ~ line 15 ~ QuizPage ~ answers", answers);
 
   const { id } = useParams();
   const history = useHistory();
@@ -72,7 +73,44 @@ const QuizPage = () => {
     };
     setAnswers({ ...answers, [idx]: answer });
   };
+  function areEqual(array1, array2) {
+    if (array1.length === array2.length) {
+      return array1.every((element) => {
+        if (array2.includes(element)) {
+          return true;
+        }
 
+        return false;
+      });
+    }
+
+    return false;
+  }
+
+  const score = () => {
+    if (result) {
+      let all = res?.exam.questions.length;
+      let correct = Object.values(answers).map((answer, idx) => {
+        let job = res.exam.questions[idx].answer;
+
+        if (typeof answer === "object") {
+          let check = areEqual(job, answer);
+
+          return check === true ? 1 : 0;
+        } else {
+          return Number(job[0]) === Number(answer) ? 1 : 0;
+        }
+      });
+
+      if (correct.length > 0)
+        return `نمره محاسبه شده  :  ${correct.reduce(
+          (t, a) => t + a
+        )}/${all} ،  ${(parseFloat((correct.reduce((t, a) => t + a) / all)).toPrecision(1)) * 10} از 10 `;
+      else return "شما پاسخی ثبت نکرده اید.";
+    } else {
+      return 0;
+    }
+  };
   return (
     <section className="max-full mx-5 my-3 sm:px-5 rounded bg-white ">
       {!data ? (
@@ -93,9 +131,29 @@ const QuizPage = () => {
             {data.quiz.name}{" "}
           </div>
           <h2>آزمونک</h2>
+          {result && (
+            <div class="flex bg-green-200 w-100 mb-4">
+              <div class="w-16 bg-green-400">
+                <div class="p-4">
+                  <svg
+                    class="h-8 w-8 text-white fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M468.907 214.604c-11.423 0-20.682 9.26-20.682 20.682v20.831c-.031 54.338-21.221 105.412-59.666 143.812-38.417 38.372-89.467 59.5-143.761 59.5h-.12C132.506 459.365 41.3 368.056 41.364 255.883c.031-54.337 21.221-105.411 59.667-143.813 38.417-38.372 89.468-59.5 143.761-59.5h.12c28.672.016 56.49 5.942 82.68 17.611 10.436 4.65 22.659-.041 27.309-10.474 4.648-10.433-.04-22.659-10.474-27.309-31.516-14.043-64.989-21.173-99.492-21.192h-.144c-65.329 0-126.767 25.428-172.993 71.6C25.536 129.014.038 190.473 0 255.861c-.037 65.386 25.389 126.874 71.599 173.136 46.21 46.262 107.668 71.76 173.055 71.798h.144c65.329 0 126.767-25.427 172.993-71.6 46.262-46.209 71.76-107.668 71.798-173.066v-20.842c0-11.423-9.259-20.683-20.682-20.683z" />
+                    <path d="M505.942 39.803c-8.077-8.076-21.172-8.076-29.249 0L244.794 271.701l-52.609-52.609c-8.076-8.077-21.172-8.077-29.248 0-8.077 8.077-8.077 21.172 0 29.249l67.234 67.234a20.616 20.616 0 0 0 14.625 6.058 20.618 20.618 0 0 0 14.625-6.058L505.942 69.052c8.077-8.077 8.077-21.172 0-29.249z" />
+                  </svg>
+                </div>
+              </div>
+              <div class="w-auto text-grey-darker items-center p-4">
+                <span class="text-lg font-bold pb-4">محاسبه نمره شما</span>
+                <p class="leading-tight">{score()}</p>
+              </div>
+            </div>
+          )}
           {res ? (
             res?.exam.questions?.map((item, idx) => (
-              <div key={"q" + idx} className="flex col my-4 mx-1 border-b ">
+              <div key={"q" + idx} className="flex col my-6 mx-1 py-2 border-b ">
                 <span>{idx + 1 + "-"} </span> {GetQuestion(item, idx)}
               </div>
             ))
@@ -107,11 +165,21 @@ const QuizPage = () => {
             </div>
           )}
           {result ? (
-            <Link to={`/lectures/${+id + 1}/0`}>
-              <button className="w-200 m-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">
-                مبحث بعدی{" "}
+            <div>
+              <Link to={`/lectures/${+id + 1}/0`}>
+                <button className="w-200 m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                  مبحث بعدی{" "}
+                </button>
+              </Link>
+              <button
+                className="w-200 m-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded"
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                شرکت مجدد{" "}
               </button>
-            </Link>
+            </div>
           ) : (
             <button
               className="w-200 m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
--- src/components/pages/Quiz/types/MultiSelect.js
+++ src/components/pages/Quiz/types/MultiSelect.js
@@ -3,7 +3,6 @@ import Latex from "../../../hooks/LATEX";
 
 const MultiSelect = ({ item, idx, answers, SetUserAnswer, result }) => {
   const changeAnswer = (e) => {
-    e.preventDefault();
     let pre = answers[idx] ?? [];
     if (pre.includes(e)) {
       pre = pre.filter(function (ele) {
@@ -18,13 +17,21 @@ const MultiSelect = ({ item, idx, answers, SetUserAnswer, result }) => {
     <div className="justify-right mx-2">
       <Latex>
         <div
-          className="text-justify"
-          dangerouslySetInnerHTML={{ __html: item.title }}
+          className="text-justify mb-3"
+          dangerouslySetInnerHTML={{ __html: item.question }}
         />
       </Latex>
       <form>
         {item.options.map((q, id) => (
           <div key={"check" + id} className="form-check flex items-center">
+            <span className="text-green-500">
+              {result && item.answer.includes(id + 1) && (
+                <img
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABvElEQVQ4je3Sz0vTcRzH8edn38220oKQQEETUiiSRPBi/sCRFiKtFQw6SSgadOtQRn9B6FGoQ4cReNHIg23MSaAEViPoEEjrkJHVQZh50HDf7dPn3SF/fJ0zzN2k1+nzeX3enwefL3zhfwqNKuRy/+fLQQN+n9Z3h2tidkFg35cr7YiJAkXAtOewN/DwxNNV136wmwuBBsSMr2MAbdl0+gzAP4O9XwPVxkgEKNnoFOrO48rnb/+s1xOSkFU8vzKEIR6umYznw3qSHeXitmaBqq1WDYarYwMbu80XepOpIZ21b+tfdrT7o/9WLhb61H4sg45qbVdpbaO1TVZnRsKnYveccwrgerKpXLLmO9tPHoyefX0fhYReNfqsEjUlSLNjIrKYOnR1xj+jd4AAwXf1vUp4BHgch88sb+aGThc9Aa5t9ZJYdZsLU3Xvf+Z+ybbfpitx+hLCGHDUUaeAUsd+ThurNX5+7kcutgMEuDhdUSuWKwKczDP/TVxW04vm+YV8WF4QoGWytMzlVhNAg6NeEjEtLzuWPuyG7QoCnItzxKeLRxQSBJbBdL7pWkv8DdtT6sc9dY1jHC8YOrj5DfC2k/QySZIkAAAAAElFTkSuQmCC"
+                  alt=""
+                />
+              )}
+            </span>
             <input
               disabled={result}
               className="form-check-input appearance-none h-4 w-4 border border-gray-000 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
@@ -46,8 +53,8 @@ const MultiSelect = ({ item, idx, answers, SetUserAnswer, result }) => {
         <Latex>
           <h3>جواب صحیح</h3>
           <div
-            className="text-justify"
-            dangerouslySetInnerHTML={{ __html: item.correct }}
+            className="text-justify text-green-600 my-3"
+            dangerouslySetInnerHTML={{ __html: item.explain }}
           />
         </Latex>
       )}
